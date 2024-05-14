104b434019d055a1e4c880d6b0e5e6799cce8157
Hadi <Hadi69ans@gmail.com>
2024-05-14 20:20:19 +0330

    add base folder

---
+++ Ham30.Server/Entites/Contracts/AuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ham30.Server.Domain.Contracts
+{
+    public abstract class AuditableEntity<TId> : IAuditableEntity<TId>
+    {
+        public TId Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+        public string DeletedBy { get; set; }
+        public DateTime? DeletedOn { get; set; }
+
+    }
+}
